36af032132739483e3545a09029638d78d85b173
najmanmna <139351540+najmanmna@users.noreply.github.com>
2025-10-06 22:13:16 +0530

    what if

--- components/WhatIs.tsx
+++ components/WhatIs.tsx
@@ -20,41 +20,23 @@ const WhatIs = () => {
       </div>
 
       <div className="flex flex-col lg:flex-row items-center gap-10">
-        <div className="relative w-full lg:w-1/2">
-  {/* Tiled border using pseudo-elements */}
-  <div className="absolute inset-0 pointer-events-none">
-    {/* Top Border */}
-    <div
-      className="absolute top-10 left-0 w-full h-16 bg-repeat-x"
-      style={{ backgroundImage: `url(${borderTile.src})` }}
-    />
-    {/* Bottom Border */}
-    <div
-      className="absolute bottom-0 left-0 w-full h-16 bg-repeat-x"
-      style={{ backgroundImage: `url(${borderTile.src})` }}
-    />
-    {/* Left Border */}
-    <div
-      className="absolute top-0 left-0 w-16 h-full bg-repeat-y"
-      style={{ backgroundImage: `url(${borderTile.src})` }}
-    />
-    {/* Right Border */}
-    <div
-      className="absolute top-0 right-0 w-16 h-full bg-repeat-y"
-      style={{ backgroundImage: `url(${borderTile.src})` }}
-    />
-  </div>
-
-  {/* Main Image */}
-  <Image
-    src={heroImage}
-    alt="Block Printing"
-    width={600}
-    height={600}
-    className="w-full h-auto object-cover relative z-10"
-  />
-</div>
-
+        {/* Image with Tiled Border */}
+        <div
+          className="p-[10px] relative"
+          style={{
+            backgroundImage: `url(${borderTile.src})`,
+            backgroundRepeat: "repeat",
+            backgroundSize: "10%", // just like Figma’s "Tile 10%"
+          }}
+        >
+          <Image
+            src={heroImage}
+            alt="Block Printing"
+            width={600}
+            height={600}
+            className="w-full h-auto object-cover block"
+          />
+        </div>
 
         {/* Right Content */}
         <div className="flex-1 text-center lg:text-left">
@@ -68,13 +50,13 @@ const WhatIs = () => {
           <div className="mt-6 flex justify-center lg:justify-start gap-4">
             <a
               href="/our-story"
-              className="px-6 py-3 bg-tech_primary text-white font-semibold rounded shadow hover:bg-tech_dark transition-colors"
+              className="px-6 py-3 bg-tech_primary text-white font-semibold rounded shadow hover:bg-tech_gold hover:text-primary transition-colors"
             >
               READ OUR STORY
             </a>
             <a
               href="/how-to-care"
-              className="px-6 py-3 border border-tech_primary text-tech_primary font-semibold rounded shadow hover:bg-tech_primary hover:text-white transition-colors"
+              className="px-6 py-3 border border-tech_gold text-tech_primary font-semibold rounded shadow hover:bg-tech_gold hover:text-white transition-colors"
             >
               HOW TO CARE
             </a>
